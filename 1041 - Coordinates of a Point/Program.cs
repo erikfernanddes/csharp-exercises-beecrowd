d6497f4be8b44a1f1a1b6ad4d0d056b77b1804cf
Erick Fernandes <oerickfernandes@gmail.com>
2023-01-26 17:25:56 -0300

    Resolução 1041

---
+++ 1041 - Coordinates of a Point/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] posicaoEixosXY = Console.ReadLine().Split(' ');
+            double eixoX = Convert.ToDouble(posicaoEixosXY[0]);
+            double eixoY = Convert.ToDouble(posicaoEixosXY[1]);
+
+            if(eixoX == 0 & eixoY == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if(eixoX != 0 & eixoY == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if(eixoX == 0 & eixoY != 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if(eixoX > 0 && eixoY > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if(eixoX > 0 && eixoY < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if(eixoX < 0 && eixoY > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if(eixoX < 0 && eixoY < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            Console.Read();
+        }
+    }
+}
