356e91cfe7bc0f37db31105315fda0f25ebc0e01
Erick Fernandes <oerickfernandes@gmail.com>
2023-01-27 19:00:46 -0300

    Resolução 1043

---
+++ 1043 - Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] valores = Console.ReadLine().Split(' ');
+            double ladoA = Convert.ToDouble(valores[0],CultureInfo.InvariantCulture);
+            double ladoB = Convert.ToDouble(valores[1],CultureInfo.InvariantCulture);
+            double ladoC = Convert.ToDouble(valores[2],CultureInfo.InvariantCulture);
+
+            if ( ladoA < ladoB + ladoC && ladoB < ladoA + ladoC && ladoC < ladoA + ladoB)
+            {
+                double perimetro = ladoA + ladoB + ladoC;
+                Console.WriteLine($"Perimetro = {perimetro.ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                double area = ((ladoA + ladoB) * ladoC) / 2;
+                Console.WriteLine($"Area = {area.ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
