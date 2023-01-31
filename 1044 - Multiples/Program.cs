e5fea685b9e21c0e6f235d5b0bef38f1a8ff4c3a
Erick Fernandes <oerickfernandes@gmail.com>
2023-01-31 14:41:53 -0300

    Resolução 1044

---
+++ 1044 - Multiples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int primeiroValor = Convert.ToInt32(valores[0]);
+            int segundoValor = Convert.ToInt32(valores[1]);
+
+            if(primeiroValor % segundoValor == 0 || segundoValor % primeiroValor == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+        }
+    }
+}
