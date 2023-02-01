fed443fcaadd8387507f56fbd967e2ed59225df5
Erick Fernandes <oerickfernandes@gmail.com>
2023-02-01 16:40:34 -0300

    Resolução 1046

---
+++ 1046 - Game Time/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Beecrowd1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horarioInicial, horarioFinal, duracao;
+            
+            string[] horario = Console.ReadLine().Split(' ');
+            horarioInicial = Convert.ToInt32(horario[0]);
+            horarioFinal = Convert.ToInt32(horario[1]);
+
+            if(horarioInicial >= horarioFinal)
+            {
+                duracao = (horarioFinal + 24) - horarioInicial;
+            }
+            else
+            {
+                duracao = horarioFinal - horarioInicial;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+            Console.Read();
+        }
+    }
+}
