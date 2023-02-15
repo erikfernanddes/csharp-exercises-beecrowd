fe403917f16adff02bc35f9c97b7ed1f0c1c3aad
Erick Fernandes <oerickfernandes@gmail.com>
2023-02-15 12:57:02 -0300

    Resolução 1049

---
+++ 1049 - Animal/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Beecrowd1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string grupoFilo = Console.ReadLine().ToLower();
+            string classe = Console.ReadLine().ToLower();
+            string caracteristica = Console.ReadLine().ToLower();
+            string animal = "";
+
+            if(grupoFilo == "vertebrado")
+            {
+                if(classe == "ave")
+                {
+                    if(caracteristica == "carnivoro")
+                    {
+                        animal = "aguia";
+                    }
+                    else if(caracteristica == "onivoro")
+                    {
+                        animal = "pomba";
+                    }
+                }
+                else if(classe == "mamifero")
+                {
+                    if(caracteristica == "onivoro")
+                    {
+                        animal = "homem";
+                    }
+                    else if(caracteristica == "herbivoro")
+                    {
+                        animal = "vaca";
+                    }
+                }
+            }
+            else if(grupoFilo == "invertebrado")
+            {
+                if(classe == "inseto")
+                {
+                    if(caracteristica == "hematofago")
+                    {
+                        animal = "pulga";
+                    }
+                    else if(caracteristica == "herbivoro")
+                    {
+                        animal = "lagarta";
+                    }
+                }
+                else if (classe == "anelideo")
+                {
+                    if(caracteristica == "hematofago")
+                    {
+                        animal = "sanguessuga";
+                    }
+                    else if(caracteristica == "onivoro")
+                    {
+                        animal = "minhoca";
+                    }
+                }
+            }
+
+            Console.WriteLine(animal);
+            Console.Read();
+
+        }
+    }
+}
