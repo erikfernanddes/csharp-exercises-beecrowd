2ad5a9d334eec89445996b8d3a5c702d847bfe44
Erick Fernandes <oerickfernandes@gmail.com>
2023-02-20 19:33:50 -0300

    Resolução 1059

---
+++ 1059 - Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Beecrowd1059
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int cont = 1; cont <= 100; cont++)
+            {
+                if(cont % 2 == 0)
+                {
+                    Console.WriteLine(cont);
+                }
+            }
+
+        }
+    }
+}
