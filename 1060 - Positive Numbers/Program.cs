9e343d156e3a309072ecdb9bb73215a1890f067a
Erick Fernandes <oerickfernandes@gmail.com>
2023-02-24 17:34:38 -0300

    Resolução 1060

---
+++ 1060 - Positive Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1060
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+
+            int contador = 0, totalPositivos = 0;
+            double numero;
+
+            while(contador < 6)
+            {
+                numero = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(numero > 0)
+                {
+                    totalPositivos += 1;
+                }
+
+                contador++;
+            }
+
+            Console.WriteLine($"{totalPositivos} valores positivos");
+
+        }
+    }
+}
