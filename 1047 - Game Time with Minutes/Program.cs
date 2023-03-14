512c8c6e021a050fac37425fa79ce63a7db5fcba
Erick Fernandes <oerickfernandes@gmail.com>
2023-03-14 13:30:57 -0300

    Resoluções 1047 e 1051

---
+++ 1047 - Game Time with Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Beecrowd1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tempoDeJogo = Console.ReadLine().Split(' ');
+            int horaInicial = Convert.ToInt32(tempoDeJogo[0]);
+            int minutoInicial = Convert.ToInt32(tempoDeJogo[1]);
+            int horaFinal = Convert.ToInt32(tempoDeJogo[2]);
+            int minutoFinal = Convert.ToInt32(tempoDeJogo[3]);
+
+            int inicioEmMinutos = (horaInicial * 60) + minutoInicial;
+            int finalEmMinutos = (horaFinal * 60) + minutoFinal;
+            int duracaoPartida;
+
+            if(inicioEmMinutos < finalEmMinutos)
+            {
+                duracaoPartida = finalEmMinutos - inicioEmMinutos;
+            }
+            else
+            {
+                duracaoPartida = (24 * 60 - inicioEmMinutos) + finalEmMinutos;
+            }
+            
+            int duracaoHoras = duracaoPartida / 60;
+            int duracaoMinutos = duracaoPartida % 60;
+
+            Console.WriteLine($"O JOGO DUROU {duracaoHoras} HORA(S) E {duracaoMinutos} MINUTO(S)");
+
+        }
+    }
+}
---
+++ 1051 - Taxes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Beecrowd1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salario = Convert.ToDouble(Console.ReadLine());
+            double taxa = 0.00;
+            bool isento = salario < 2000.00;
+
+            if(salario > 2000.00 && salario <= 3000.00)
+            {
+                taxa =  (salario - 2000.00) * 8 / 100;
+            } 
+            else if(salario > 3000.00 && salario < 4500.00)
+            {
+                taxa = (salario - 3000.00) * 18 / 100 + 80.00; //Imposto de 8% sobre 1000;
+            }
+            else
+            {
+                taxa = ((salario - 4500.0) * 28 / 100) + (1500.0 * 18 / 100) + (1000.0 * 8 / 100);
+            }
+
+            Console.WriteLine((isento ? "Isento" : $"R$ {taxa:F2}"));
+        
+        }
+    }
+}
