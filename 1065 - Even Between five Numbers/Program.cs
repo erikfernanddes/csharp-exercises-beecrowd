4b1b08d3c4079238a41c65ceb1f5eca2f091cd91
Erick Fernandes <oerickfernandes@gmail.com>
2023-03-15 19:05:52 -0300

    Resolução 1065

---
+++ 1065 - Even Between five Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1, totalPares = 0, numero;
+
+            while(contador <= 5)
+            {
+                numero = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(numero % 2 == 0)
+                {
+                    totalPares += 1;
+                }
+
+                contador++;
+            }
+
+            Console.WriteLine($"{totalPares} valores pares");
+        }
+    }
+}
