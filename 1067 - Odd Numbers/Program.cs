dfd82f7a48286be8f98765779f3ec589dae6398a
Erick Fernandes <oerickfernandes@gmail.com>
2023-03-19 15:00:50 -0300

    Resolução 10067

---
+++ 1067 - Odd Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numero = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            for(int contador = 0; contador <= numero; contador++)
+            {              
+                if(contador % 2 != 0)
+                {
+                    Console.WriteLine(contador);
+                }
+            }
+
+        }
+    }
+}
