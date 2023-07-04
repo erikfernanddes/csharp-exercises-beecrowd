91c1b5dfb91fdef6ccf568cb81a2a19ddfe5930a
Erik Fernandes <oerickfernandes@gmail.com>
2023-07-04 15:42:14 -0300

    Resolução 1096

---
+++ Beginner/1096 - Sequence IJ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd1096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i=1, j=7;
+
+            for(int contador = i; contador <= 9; contador+=2)
+            {
+                for(int cont = j; cont >= 5; cont--)
+                {
+                    Console.WriteLine($"I={contador} J={cont}");
+                }
+                j=7;
+            }
+
+        }
+    }   
+}
